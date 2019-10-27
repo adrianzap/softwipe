ef9effa71b2d8415a5aba21b440c395b5b215130
adrianzap <adrianzap24@gmail.com>
2019-10-27 11:15:29 +0100

    Add option to create absolute table in calculate_score_table

--- calculate_score_table.py
+++ calculate_score_table.py
@@ -29,6 +29,8 @@ def parse_arguments():
                                                           'a folder should exist in this directory that contains the '
                                                           'output of softwipe in a file called '
                                                           '"' + SOFTWIPE_OUTPUT_FILE_NAME + '"')
+    parser.add_argument('-A', '--absolute', action='store_true', help='create a table with absolute values rather '
+                                                                      'than scores')
     args = parser.parse_args()
     return args
 
@@ -72,19 +74,77 @@ def get_result_rates(result_directory, folder):
         unique_rate, kwstyle_rate
 
 
-def calculate_scores(result_directory):
+def get_result_values(result_directory, folder):
+    def get_absolute_value(abs_by_loc):
+        return int(abs_by_loc.split('/')[0][1:])
+
+    cur_folder = os.path.join(result_directory, folder)
+    cur_file = os.path.join(cur_folder, SOFTWIPE_OUTPUT_FILE_NAME)
+    cur_lines = open(cur_file, 'r').readlines()  # Softwipe output lines
+
     # Init
-    scores = {
-        'compiler_and_sanitizer': {},
-        'assertions': {},
-        'cppcheck': {},
-        'clang_tidy': {},
-        'cyclomatic_complexity': {},
-        'lizard_warnings': {},
-        'unique': {},
-        'kwstyle': {},
-        'overall': {}
-    }
+    sanitizer_warnings = 0
+    loc = functions = compiler_warnings = assertions = cppcheck_warnings = clang_tidy_warnings = \
+        ccn = lizard_warnings = unique = kwstyle_warnings = None
+
+    # Iterate through the softwipe output
+    for line in cur_lines:
+        split_line = line.split()
+
+        if line.startswith('Lines of pure'):
+            loc = int(split_line[-1])
+        elif line.startswith('Weighted compiler warning rate:'):
+            compiler_warnings = get_absolute_value(split_line[-1])
+        elif line.startswith(('AddressSanitizer error rate:', 'UndefinedBehaviorSanitizer error rate:')):
+            sanitizer_warnings += get_absolute_value(split_line[-1])
+        elif line.startswith('Assertion rate:'):
+            assertions = get_absolute_value(split_line[-1])
+        elif line.startswith('Total weighted Cppcheck warning rate:'):
+            cppcheck_warnings = get_absolute_value(split_line[-1])
+        elif line.startswith('Weighted Clang-tidy warning rate:'):
+            clang_tidy_warnings = get_absolute_value(split_line[-1])
+        elif line.startswith('Average cyclomatic complexity:'):
+            ccn = float(split_line[-1])
+        elif line.startswith('Lizard warning rate (~= rate of functions that are too complex):'):
+            functions = int(split_line[-1].split('/')[1][:-1])
+            lizard_warnings = get_absolute_value(split_line[-1])
+        elif line.startswith('Unique code rate:'):
+            unique = float(split_line[-1])
+        elif line.startswith('KWStyle warning rate:'):
+            kwstyle_warnings = get_absolute_value(split_line[-1])
+
+    return loc, functions, compiler_warnings, sanitizer_warnings, assertions, cppcheck_warnings, clang_tidy_warnings, \
+                ccn, lizard_warnings, unique, kwstyle_warnings
+
+
+def calculate_scores(result_directory, absolute):
+    # Init
+    if absolute:
+        scores = {
+            'loc': {},
+            'functions': {},
+            'compiler': {},
+            'sanitizer': {},
+            'assertions': {},
+            'cppcheck': {},
+            'clang_tidy': {},
+            'cyclomatic_complexity': {},
+            'lizard_warnings': {},
+            'unique': {},
+            'kwstyle': {}
+        }
+    else:
+        scores = {
+            'compiler_and_sanitizer': {},
+            'assertions': {},
+            'cppcheck': {},
+            'clang_tidy': {},
+            'cyclomatic_complexity': {},
+            'lizard_warnings': {},
+            'unique': {},
+            'kwstyle': {},
+            'overall': {}
+        }
 
     for score in scores:
         for folder in FOLDERS:
@@ -92,39 +152,57 @@ def calculate_scores(result_directory):
 
     # Get all the scores
     for folder in FOLDERS:
-        # Get rates
-        compiler_and_sanitizer_rate, assertion_rate, cppcheck_rate, clang_tidy_rate, ccn, lizard_rate, unique_rate, \
-            kwstyle_rate = get_result_rates(result_directory, folder)
+        if absolute:
+            # Get values
+            loc, functions, compiler_warnings, sanitizer_warnings, assertions, cppcheck_warnings, clang_tidy_warnings, \
+                ccn, lizard_warnings, unique, kwstyle_warnings = get_result_values(result_directory, folder)
+            scores['loc'][folder] = loc
+            scores['functions'][folder] = functions
+            scores['compiler'][folder] = compiler_warnings
+            scores['sanitizer'][folder] = sanitizer_warnings
+            scores['assertions'][folder] = assertions
+            scores['cppcheck'][folder] = cppcheck_warnings
+            scores['clang_tidy'][folder] = clang_tidy_warnings
+            scores['cyclomatic_complexity'][folder] = ccn
+            scores['lizard_warnings'][folder] = lizard_warnings
+            scores['unique'][folder] = unique
+            scores['kwstyle'][folder] = kwstyle_warnings
+        else:
+            # Get rates
+            compiler_and_sanitizer_rate, assertion_rate, cppcheck_rate, clang_tidy_rate, ccn, lizard_rate, \
+                unique_rate, kwstyle_rate = get_result_rates(result_directory, folder)
 
-        # Get scores
-        scores['compiler_and_sanitizer'][folder] = scoring.calculate_compiler_and_sanitizer_score(
-            compiler_and_sanitizer_rate)
-        scores['assertions'][folder] = scoring.calculate_assertion_score(assertion_rate)
-        scores['cppcheck'][folder] = scoring.calculate_cppcheck_score(cppcheck_rate)
-        scores['clang_tidy'][folder] = scoring.calculate_clang_tidy_score(clang_tidy_rate)
-        scores['cyclomatic_complexity'][folder] = scoring.calculate_cyclomatic_complexity_score(ccn)
-        scores['lizard_warnings'][folder] = scoring.calculate_lizard_warning_score(lizard_rate)
-        scores['unique'][folder] = scoring.calculate_unique_score(unique_rate)
-        scores['kwstyle'][folder] = scoring.calculate_kwstyle_score(kwstyle_rate)
+            # Get scores
+            scores['compiler_and_sanitizer'][folder] = scoring.calculate_compiler_and_sanitizer_score(
+                compiler_and_sanitizer_rate)
+            scores['assertions'][folder] = scoring.calculate_assertion_score(assertion_rate)
+            scores['cppcheck'][folder] = scoring.calculate_cppcheck_score(cppcheck_rate)
+            scores['clang_tidy'][folder] = scoring.calculate_clang_tidy_score(clang_tidy_rate)
+            scores['cyclomatic_complexity'][folder] = scoring.calculate_cyclomatic_complexity_score(ccn)
+            scores['lizard_warnings'][folder] = scoring.calculate_lizard_warning_score(lizard_rate)
+            scores['unique'][folder] = scoring.calculate_unique_score(unique_rate)
+            scores['kwstyle'][folder] = scoring.calculate_kwstyle_score(kwstyle_rate)
 
-        # Calculate the overall score
-        list_of_scores = [scores[score][folder] for score in scores.keys() if score != 'overall']
-        scores['overall'][folder] = scoring.average_score(list_of_scores)
+            # Calculate the overall score
+            list_of_scores = [scores[score][folder] for score in scores.keys() if score != 'overall']
+            scores['overall'][folder] = scoring.average_score(list_of_scores)
 
     return scores
 
 
-def print_score_csv(scores):
+def print_score_csv(scores, absolute):
+    last_column = 'kwstyle' if absolute else 'overall'
+
     print('program', end=',')
     for score in scores:
-        if score != 'overall':
+        if score != last_column:
             print(score, end=',')
         else:
             print(score)
     for folder in FOLDERS:
         print(folder, end=',')
         for score in scores:
-            if score != 'overall':
+            if score != last_column:
                 print(scores[score][folder], end=',')
             else:
                 print(scores[score][folder])
@@ -133,8 +211,9 @@ def print_score_csv(scores):
 def main():
     args = parse_arguments()
     result_directory = os.path.abspath(args.result_directory[0])
-    scores = calculate_scores(result_directory)
-    print_score_csv(scores)
+    absolute = True if args.absolute else False
+    scores = calculate_scores(result_directory, absolute)
+    print_score_csv(scores, absolute)
 
 
 if __name__ == "__main__":
